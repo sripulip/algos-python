5076487edba159ec7255564cbaa98fef43f0948b
Sriram Pulipaka <sriram.pulipaka@dealertrack.com>
2018-08-05 20:20:01 -0400

    Added file

---
+++ problem_solving/reverse_integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverse(self, x):
+        print(x)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    solution.reverse(123)
+    solution.reverse(-123)
+    solution.reverse(120)
